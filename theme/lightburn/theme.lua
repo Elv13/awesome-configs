75be5f1a2a4bf20f924fe7ff7f07b4804469315a
Emmanuel Lepage Vallee <elv1313@gmail.com>
2011-05-21 23:03:35 -0400

    Path bug

--- theme/lightburn/theme.lua
+++ theme/lightburn/theme.lua
@@ -7,7 +7,7 @@
 -- {{{ Main
 theme = {}
 -- theme.wallpaper_cmd = { "awsetbg ".. os.getenv('HOME').."/.config/awesome/lightburn/lightburn-background.png" }
-theme.wallpaper_cmd = { "feh --bg-scale "..awful.util.getdir("config").."/lightburn/lightburn-background.png" }
+theme.wallpaper_cmd = { "feh --bg-scale "..awful.util.getdir("config").."/theme/lightburn/lightburn-background.png" }
 -- }}}
 
 -- {{{ Styles
@@ -73,60 +73,60 @@ theme.menu_width  = "100"
 
 -- {{{ Icons
 -- {{{ Taglist
-theme.taglist_squares_sel   = awful.util.getdir("config").."/lightburn/taglist/squarefz.png"
-theme.taglist_squares_unsel = awful.util.getdir("config").."/lightburn/taglist/squarez.png"
+theme.taglist_squares_sel   = awful.util.getdir("config").."/theme/lightburn/taglist/squarefz.png"
+theme.taglist_squares_unsel = awful.util.getdir("config").."/theme/lightburn/taglist/squarez.png"
 --theme.taglist_squares_resize = "false"
 -- }}}
 
 -- {{{ Misc
-theme.awesome_icon           = awful.util.getdir("config").."/lightburn/awesome-icon.png"
+theme.awesome_icon           = awful.util.getdir("config").."/theme/lightburn/awesome-icon.png"
 theme.menu_submenu_icon      = awful.util.getdir("config").."/default/submenu.png"
 theme.tasklist_floating_icon = awful.util.getdir("config").."/default/tasklist/floatingw.png"
 -- }}}
 
 -- {{{ Layout
-theme.layout_tile       = awful.util.getdir("config").."/lightburn/layouts/tile.png"
-theme.layout_tileleft   = awful.util.getdir("config").."/lightburn/layouts/tileleft.png"
-theme.layout_tilebottom = awful.util.getdir("config").."/lightburn/layouts/tilebottom.png"
-theme.layout_tiletop    = awful.util.getdir("config").."/lightburn/layouts/tiletop.png"
-theme.layout_fairv      = awful.util.getdir("config").."/lightburn/layouts/fairv.png"
-theme.layout_fairh      = awful.util.getdir("config").."/lightburn/layouts/fairh.png"
-theme.layout_spiral     = awful.util.getdir("config").."/lightburn/layouts/spiral.png"
-theme.layout_dwindle    = awful.util.getdir("config").."/lightburn/layouts/dwindle.png"
-theme.layout_max        = awful.util.getdir("config").."/lightburn/layouts/max.png"
-theme.layout_fullscreen = awful.util.getdir("config").."/lightburn/layouts/fullscreen.png"
-theme.layout_magnifier  = awful.util.getdir("config").."/lightburn/layouts/magnifier.png"
-theme.layout_floating   = awful.util.getdir("config").."/lightburn/layouts/floating.png"
+theme.layout_tile       = awful.util.getdir("config").."/theme/lightburn/layouts/tile.png"
+theme.layout_tileleft   = awful.util.getdir("config").."/theme/lightburn/layouts/tileleft.png"
+theme.layout_tilebottom = awful.util.getdir("config").."/theme/lightburn/layouts/tilebottom.png"
+theme.layout_tiletop    = awful.util.getdir("config").."/theme/lightburn/layouts/tiletop.png"
+theme.layout_fairv      = awful.util.getdir("config").."/theme/lightburn/layouts/fairv.png"
+theme.layout_fairh      = awful.util.getdir("config").."/theme/lightburn/layouts/fairh.png"
+theme.layout_spiral     = awful.util.getdir("config").."/theme/lightburn/layouts/spiral.png"
+theme.layout_dwindle    = awful.util.getdir("config").."/theme/lightburn/layouts/dwindle.png"
+theme.layout_max        = awful.util.getdir("config").."/theme/lightburn/layouts/max.png"
+theme.layout_fullscreen = awful.util.getdir("config").."/theme/lightburn/layouts/fullscreen.png"
+theme.layout_magnifier  = awful.util.getdir("config").."/theme/lightburn/layouts/magnifier.png"
+theme.layout_floating   = awful.util.getdir("config").."/theme/lightburn/layouts/floating.png"
 -- }}}
 
 -- {{{ Titlebar
-theme.titlebar_close_button_focus  = awful.util.getdir("config").."/lightburn/titlebar/close_focus.png"
-theme.titlebar_close_button_normal = awful.util.getdir("config").."/lightburn/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = awful.util.getdir("config").."/theme/lightburn/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = awful.util.getdir("config").."/theme/lightburn/titlebar/close_normal.png"
 
--- theme.titlebar_ontop_button_focus_active  = awful.util.getdir("config").."/lightburn/titlebar/ontop_focus_active.png"
--- theme.titlebar_ontop_button_normal_active = awful.util.getdir("config").."/lightburn/titlebar/ontop_normal_active.png"
--- theme.titlebar_ontop_button_focus_inactive  = awful.util.getdir("config").."/lightburn/titlebar/ontop_focus_inactive.png"
--- theme.titlebar_ontop_button_normal_inactive = awful.util.getdir("config").."/lightburn/titlebar/ontop_normal_inactive.png"
+-- theme.titlebar_ontop_button_focus_active  = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_focus_active.png"
+-- theme.titlebar_ontop_button_normal_active = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_normal_active.png"
+-- theme.titlebar_ontop_button_focus_inactive  = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_focus_inactive.png"
+-- theme.titlebar_ontop_button_normal_inactive = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_normal_inactive.png"
 
-theme.titlebar_ontop_button_focus_active  = awful.util.getdir("config").."/lightburn/titlebar/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active = awful.util.getdir("config").."/lightburn/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive  = awful.util.getdir("config").."/lightburn/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive = awful.util.getdir("config").."/lightburn/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_active  = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = awful.util.getdir("config").."/theme/lightburn/titlebar/ontop_normal_inactive.png"
 
-theme.titlebar_sticky_button_focus_active  = awful.util.getdir("config").."/lightburn/titlebar/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active = awful.util.getdir("config").."/lightburn/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive  = awful.util.getdir("config").."/lightburn/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive = awful.util.getdir("config").."/lightburn/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active  = awful.util.getdir("config").."/theme/lightburn/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = awful.util.getdir("config").."/theme/lightburn/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = awful.util.getdir("config").."/theme/lightburn/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = awful.util.getdir("config").."/theme/lightburn/titlebar/sticky_normal_inactive.png"
 
-theme.titlebar_floating_button_focus_active  = awful.util.getdir("config").."/lightburn/titlebar/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active = awful.util.getdir("config").."/lightburn/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive  = awful.util.getdir("config").."/lightburn/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive = awful.util.getdir("config").."/lightburn/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_active  = awful.util.getdir("config").."/theme/lightburn/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = awful.util.getdir("config").."/theme/lightburn/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = awful.util.getdir("config").."/theme/lightburn/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = awful.util.getdir("config").."/theme/lightburn/titlebar/floating_normal_inactive.png"
 
-theme.titlebar_maximized_button_focus_active  = awful.util.getdir("config").."/lightburn/titlebar/maximized_focus_active.png"
-theme.titlebar_maximized_button_normal_active = awful.util.getdir("config").."/lightburn/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_inactive  = awful.util.getdir("config").."/lightburn/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_inactive = awful.util.getdir("config").."/lightburn/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active  = awful.util.getdir("config").."/theme/lightburn/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = awful.util.getdir("config").."/theme/lightburn/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = awful.util.getdir("config").."/theme/lightburn/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = awful.util.getdir("config").."/theme/lightburn/titlebar/maximized_normal_inactive.png"
 -- }}}
 -- }}}
 
