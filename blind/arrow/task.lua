5625082c87da640f5a68965327e0dc104ae97114
Emmanuel LEpage Vallee <elv1313@gmail.com>
2013-07-11 20:06:49 -0400

    Fix icons on retina display

--- blind/arrow/task.lua
+++ blind/arrow/task.lua
@@ -19,15 +19,15 @@ local function apply_icon_transformations(c)
     local ic = cairo.Surface(c.icon)
     local icp = cairo.Pattern.create_for_surface(ic)
     local sw,sh = ic:get_width(),ic:get_height()
-
+    local height = module.theme.default_height
     -- Create matrix
-    local ratio = (module.theme.default_height-2) / ((sw > sh) and sw or sh)
+    local ratio = (height-2) / ((sw > sh) and sw or sh)
     local matrix = cairo.Matrix()
     cairo.Matrix.init_scale(matrix,ratio,ratio)
-    matrix:translate(module.theme.default_height/2 - 6,-2)
+    matrix:translate(height/2 - 6,-2)
 
     --Copy to surface
-    local img5 = cairo.ImageSurface.create(cairo.Format.ARGB32, sh, sh)
+    local img5 = cairo.ImageSurface.create(cairo.Format.ARGB32, height, height)
     local cr5 = cairo.Context(img5)
     cr5:set_operator(cairo.Operator.CREAR)
     cr5:paint()
@@ -37,7 +37,7 @@ local function apply_icon_transformations(c)
     cr5:paint()
 
     --Generate the mask
-    local img4 = cairo.ImageSurface.create(cairo.Format.A8, sh, sh)
+    local img4 = cairo.ImageSurface.create(cairo.Format.A8, height, height)
     local cr4 = cairo.Context(img4)
     --cr4:set_matrix(matrix)
     cr4:set_source(icp)
