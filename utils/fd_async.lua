1714bc5b9079b437d63a480b1196987d55365621
Emmanuel LEpage Vallee <elv1313@gmail.com>
2014-01-26 17:28:17 -0500

    Add more function to df utils async

--- utils/fd_async.lua
+++ utils/fd_async.lua
@@ -5,9 +5,37 @@
 
 local setmetatable = setmetatable
 local ipairs = ipairs
+local pairs = pairs
+local tostring = tostring
 local gio = require("lgi").Gio
+local util = require("awful.util")
 
 local module = {}
+
+---------------------------------------------------------------------
+---                           HELPERS                             ---
+---------------------------------------------------------------------
+local function emit_signal(self,name,...)
+  for k,v in ipairs(self._connections[name] or {}) do
+    v(...)
+  end
+end
+
+local function connect_signal(self,name,callback)
+  self._connections[name] = self._connections[name] or {}
+  self._connections[name][#self._connections[name]+1] = callback
+end
+
+local function create_request()
+  local req = {_connections={}}
+  req.emit_signal = emit_signal
+  req.connect_signal = connect_signal
+  return req
+end
+
+---------------------------------------------------------------------
+---                           DIRECTORY                           ---
+---------------------------------------------------------------------
 
 --- Use Gio to scan a directory
 function module.scan_dir_async(path,callback,attributes,extentions)
@@ -41,20 +69,70 @@ function module.scan_dir_async(path,callback,attributes,extentions)
   end)
 end
 
---- Read all file from a directory
-function module.read_all_async(path,callback)
-  
+---------------------------------------------------------------------
+---                           Files                               ---
+---------------------------------------------------------------------
+
+-- Read a file, then call "callback" with content as first argument
+function module.load_file_async(path,callback)
+  gio.File.new_for_path(path):load_contents_async(nil,function(file,task,c)
+      local content = file:load_contents_finish(task)
+      if content then
+        callback(tostring(content))
+      end
+  end)
 end
 
--- gio.File.new_for_path('/sys/class/power_supply/'..(bat_name)..'/energy_now'):load_contents_async(nil,function(file,task,c)
---       local content = file:load_contents_finish(task)
---       if content then
---         local now = tonumber(tostring(content))
---         local percent = now/full_energy
---         percent = math.floor(percent* 100)/100
---         wdg:set_value(percent)
---       end
---   end)
+--- Read all file from a directory
+function module.load_all_async(path,args)
+  local args = args or {}
+  local req = create_request()
+  module.scan_dir_async(path,function(files)
+    local counter = 0
+    for k,v in ipairs(files) do
+      local name = v["FILE_ATTRIBUTE_STANDARD_NAME"]
+      if not args.extention or name:find("[^%s].*".. args.extention .."$") then
+        module.load_file_async(path..'/'..name,function(content)
+          req:emit_signal("file::content",path..'/'..name,content,v)
+          counter = counter - 1
+          if counter == 0 then
+            req:emit_signal("request::completed")
+          end
+        end)
+      end
+    end
+  end,args.attributes)
+  return req
+end
+
+---------------------------------------------------------------------
+---                     General file handling                     ---
+---------------------------------------------------------------------
+
+-- Parse a classical INI file format
+function module.parse_ini(content)
+  local ret = {}
+  for k,v in string.gmatch(content, "(%w+)=([^\n]+)\n?") do
+    ret[k] = v
+  end
+  return ret
+end
+
+-- Load all desktop files from a path
+function module.load_desktop_files(path,load_icon)
+  local req_in,req_out = module.load_all_async(path,{attributes={"FILE_ATTRIBUTE_STANDARD_NAME","FILE_ATTRIBUTE_STANDARD_ICON"}}),create_request()
+  req_in:connect_signal("file::content",function(name,content,attrs)
+    local ini = module.parse_ini(content)
+    if ini.Icon then
+      ini.Icon=ini.Icon:gsub("AWECFG",util.getdir("config"))
+    end
+    req_out:emit_signal("file::content",path,ini,attrs)
+  end)
+  req_in:connect_signal("request::completed",function() req_out:emit_signal("request::completed")end)
+  return req_out
+end
+
+module.load_desktop_files("/home/lepagee/.config/awesome/data/dock")
 
 return setmetatable(module, { __call = function(_, ...) return new(...) end })
 -- kate: space-indent on; indent-width 2; replace-tabs on;
