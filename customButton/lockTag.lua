b28d568b6002adb01b47cb069e8f73c4d3f1370b
Emmanuel Lepage Vallee <elv1313@gmail.com>
2014-04-16 00:00:27 -0400

    Add support for fallback and locked tags

--- customButton/lockTag.lua
+++ customButton/lockTag.lua
@@ -1,38 +1,75 @@
 local capi = {tag=tag}
 local setmetatable = setmetatable
-local print = print
 local button     = require( "awful.button"        )
 local util       = require( "awful.util"          )
 local tag        = require( "awful.tag"           )
 local config     = require( "forgotten"           )
 local themeutils = require( "blind.common.drawing")
 local wibox      = require( "wibox"               )
-local color = require("gears.color")
+local color      = require( "gears.color"         )
 
 local data = {}
 
-local function toggleVisibility(t)
+
+local function get_icon(state)
+  if state == "locked" then
+    return config.iconPath .. "tags/locked.png"
+  elseif state == "exclusive" then
+    return config.iconPath .. "tags/exclusive.png"
+  elseif state == "fallback" then
+    return config.iconPath .. "tags/fallback.png"
+  elseif state == "intrusive" then
+    return config.iconPath .. "tags/unlocked.png"
+  end
+end
+
+local function get_state(t)
+  local locked    = tag.getproperty(t,"locked") and "locked"
+  local exclusive = tag.getproperty(t,"exclusive") and "exclusive"
+  local fallback  = tag.getproperty(t,"fallback") and "fallback"
+  return fallback or locked or exclusive or "intrusive"
+end
+
+local function toggleVisibility(t,state)
   if not t or not t.selected or not data[tag.getscreen(t)] then return end
   local w = data[tag.getscreen(t)]
   if w and t.selected then
-    local locked = tag.getproperty(t,"exclusive")
-    w:set_image(color.apply_mask(locked and config.iconPath .. "tags/locked.png" or config.iconPath .. "tags/unlocked.png"))
+    w:set_image(color.apply_mask(get_icon(state or get_state(t))))
   end
+end
+
+local function next_state(t)
+  local state = get_state(t)
+  if state == "locked" then
+    tag.setproperty(t,"locked"   ,false )
+    tag.setproperty(t,"exclusive",true  )
+    tag.setproperty(t,"fallback" ,false )
+  elseif state == "exclusive" then
+    tag.setproperty(t,"locked"   ,false )
+    tag.setproperty(t,"exclusive",false )
+    tag.setproperty(t,"fallback" ,true  )
+  elseif state == "fallback" then
+    tag.setproperty(t,"locked"   ,false )
+    tag.setproperty(t,"exclusive",false )
+    tag.setproperty(t,"fallback" ,false )
+  elseif state == "intrusive" then
+    tag.setproperty(t,"locked"   ,true  )
+    tag.setproperty(t,"exclusive",false )
+    tag.setproperty(t,"fallback" ,false )
+  end
+  toggleVisibility(t)
 end
 
 local function new(screen)
   local screen = screen or 1
   if data[screen] then return data[screen] end
 
-  local lockTag,t  = wibox.widget.imagebox(),tag.selected(scree)
-  local locked = t and tag.getproperty(t,"exclusive") or false
-  lockTag:set_image(color.apply_mask(locked and config.iconPath .. "tags/locked.png" or config.iconPath .. "tags/unlocked.png"))
+  local lockTag,t  = wibox.widget.imagebox(),tag.selected(screen)
+  toggleVisibility(t)
 
   local function btn()
-    local t = tag.selected(scree)
-    local locked = not tag.getproperty(t,"exclusive")
-    lockTag:set_image(color.apply_mask(locked and config.iconPath .. "tags/locked.png" or config.iconPath .. "tags/unlocked.png"))
-    tag.setproperty(t,"exclusive", locked)
+    local t = tag.selected(screen)
+    next_state(t)
   end
 
   lockTag:buttons( util.table.join(
@@ -47,7 +84,7 @@ local function new(screen)
 end
 
 capi.tag.connect_signal("property::selected" , toggleVisibility)
-capi.tag.connect_signal("property::activated",toggleVisibility)
+capi.tag.connect_signal("property::activated", toggleVisibility)
 
 
 return setmetatable({}, { __call = function(_, ...) return new(...) end })
